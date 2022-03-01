ebc2ed42dcb7d9d67701d783e73165cc94d8034a
syahiratuzzulfa <85855859+syahiratuzzulfa@users.noreply.github.com>
2022-03-01 13:36:00 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ACER
+ * Date: 01/03/2022
+ * Time: 12:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tugas_1._
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
