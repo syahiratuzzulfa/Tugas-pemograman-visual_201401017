5ce76c9c1429b255c55a6f01091e9f1c6300ef1b
syahiratuzzulfa <85855859+syahiratuzzulfa@users.noreply.github.com>
2022-03-01 13:57:27 +0700

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ACER
+ * Date: 28/02/2022
+ * Time: 21:44
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tugas_1._
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Nama kamu siapa?");
+			
+			string nama = Console.ReadLine();
+			Console. WriteLine("Nama kamu "+nama);
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
